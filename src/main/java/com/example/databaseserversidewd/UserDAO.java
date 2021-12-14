2cbe99d1369acd318a23e29e0104e54c0a1f36b6
A00276551 <a00276551@student.ait.ie>
2021-12-14 15:28:14 +0000

    final

--- src/main/java/com/example/databaseserversidewd/UserDAO.java
+++ src/main/java/com/example/databaseserversidewd/UserDAO.java
@@ -1,16 +1,28 @@
 package com.example.databaseserversidewd;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public enum UserDAO {
 
     instance;
 
+    public void insert(User u) throws SQLException {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+//        User user = new User("name", "address");
 
-    public void insert() {
-        Class.forName("org.hsqldb.jdbcDriver");
-        Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/oneDB", "sa", "");
-        Statement s
+        Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/oneDB", "sa", "");
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate("INSERT INTO USER(name, address)" +
+                "VALUES ('" + u.getName() + "','" + u.getAddress() + "')");
+
+        stmt.close();
+        con.close();
     }
 }
